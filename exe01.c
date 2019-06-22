d8ad94ee148045ab5f898d9e17b931b0732e3092
Filipe Gabriel <filipegabrnh@gmail.com>
2019-06-22 02:37:22 -0300

    versao 1

--- exe01.c
+++ exe01.c
@@ -3,3 +3,106 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+int main()
+{
+  int pid;
+
+  pid = fork();
+
+  if (pid != 0)
+  {
+    pid = fork();
+
+    if (pid != 0)
+    {
+      printf("%d(P)1: Eu sou o pai e meu pid é %d\n", getpid(), getpid());
+      pid = wait(0);
+    }
+    else if (pid == 0)
+    {
+      pid == fork();
+
+      if(pid != 0)
+      {
+        printf("\t%d(F)2: Eu sou o filho, meu pid é %d e meu pai é %d\n", getpid(), getpid(), getppid());
+        pid = wait(0);
+      }
+
+      else if(pid == 0)
+      {
+        printf("\t\t%d(N)3: Eu sou o neto e meu pid é %d e meu pai é %d\n", getpid(), getpid(), getppid());
+        pid = wait(0);
+      }
+
+      else
+      {
+        printf("Não consigo criar filhos\n");
+        exit(-1);
+      }
+    }
+    else
+    {
+      printf("Não consigo criar filhos\n");
+      exit(-1);
+    }
+  }
+  else if(pid == 0)
+  {
+    pid = fork();
+
+    if(pid != 0)
+    {
+      pid = fork();
+
+      if(pid != 0)
+      {
+        printf("%d(F)4: Eu sou o filho, meu pid é %d e meu pai é %d\n", getpid(), getpid(), getppid());
+        pid = wait(0);
+      }
+
+      else if(pid == 0)
+      {
+        printf("%d(F)5: Eu sou o filho, meu pid é %d e meu pai é %d\n", getpid(), getpid(), getppid());
+        pid = wait(0);
+      }
+      else
+      {
+        printf("Não consigo criar filhos\n");
+        exit(-1);
+      }
+    }
+
+    else if(pid == 0)
+    {
+      pid = fork();
+
+      if(pid != 0)
+      {
+        printf("%d(F)6: Eu sou o filho, meu pid é %d e meu pai é %d\n", getpid(), getpid(), getppid());
+        pid = wait(0);
+      }
+
+      else if(pid == 0)
+      {
+        printf("%d(F)7: Eu sou o filho, meu pid é %d e meu pai é %d\n", getpid(), getpid(), getppid());
+        pid = wait(0);
+      }
+
+      else
+      {
+        printf("Não consigo criar filhos\n");
+        exit(-1);
+      }
+    }
+    else
+    {
+      printf("Não consigo criar filhos\n");
+      exit(-1);
+    }
+  }
+  else
+  {
+    printf("Não consigo criar filhos\n");
+    exit(-1);
+  }
+}
